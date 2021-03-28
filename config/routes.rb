48edf98f31fe5d024cc451d1192c5ea40146185f
Kazuki-Shimada <k-shimada@sight-visit.com>
2021-03-29 05:22:07 +0900

    add_omniauth_setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  Rails.application.routes.draw do
-    root 'application#welcome'
-  end
+  root 'application#welcome'
 end
